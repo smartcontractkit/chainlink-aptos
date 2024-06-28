06c3b0d4c5b6aaeb8acd4b572b152efa6ca44db6
Calvin <78729586+calvwang9@users.noreply.github.com>
2024-06-28 20:32:46 +1000

    Aptos MCM PoC (#19)
    * test multisig wrapper^M
    ^M
    * (wip) MCM poc^M
    ^M
    * add validations, tests, store valid_until as u64^M
    ^M
    * remove redundant multisig param^M
    ^M
    * fix test^M
    ^M
    * remove unused import^M
    ^M
    * try reorder coin test init^M
    ^M
    * bump aptos cli version in nix^M
    ^M
    * update aptos cli nix hash^M
    ^M
    * consolidate resource into mcmstate and use vector::reverse

--- aptos.nix
+++ aptos.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "aptos-${version}";
-  version = "3.1.0";
+  version = "3.4.1";
 
   src = fetchzip {
     url = "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v${version}/aptos-cli-${version}-Ubuntu-22.04-x86_64.zip";
-    sha256 = "sha256-2Qc23UhSEGXUShmZnkEDRzvSk4SmMK+FZaZpVmnLVws=";
+    sha256 = "sha256-Unq2Qu452k8EJa3F6mMJvjI5ZcVdI0nXp1YjQba6EvQ=";
   };
 
   nativeBuildInputs = [
