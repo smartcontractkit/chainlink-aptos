af809b95f3f8f15405a7c4027757d3bc678a5951
Blaž Hrastnik <blaz@mxxn.io>
2024-07-29 17:17:24 +0900

    aptos: minor: Rename feeds

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -11,8 +11,8 @@ DATA_FEEDS_ADDR=$(cat data-feeds/contract_address.txt)
 aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feed_configs" --assume-yes --args 'hex:["0x99"]' 'u256:[1]' 'u256:[60]'
 
 # data_feeds::router::set_feeds(feed_ids, descriptions, config_id, upkeep) # TODO: upkeep 0x1 for now (0x1 could maybe be circumvented?)
-aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feeds" --assume-yes --args 'hex:["0x0003111111111111111100000000000000000000000000000000000000000000"]' 'string:["ETCBTC"]' 'hex:0x99' 'address:0x1'
+aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feeds" --assume-yes --args 'hex:["0x0003111111111111111100000000000000000000000000000000000000000000"]' 'string:["FOOBAR"]' 'hex:0x99' 'address:0x1'
 
-aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feeds" --assume-yes --args 'hex:["0x0003222222222222222200000000000000000000000000000000000000000000"]' 'string:["BTCETH"]' 'hex:0x99' 'address:0x1'
+aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feeds" --assume-yes --args 'hex:["0x0003222222222222222200000000000000000000000000000000000000000000"]' 'string:["BARFOO"]' 'hex:0x99' 'address:0x1'
 
 # data_feeds::router::get_benchmarks(feed_ids, billing_data)
