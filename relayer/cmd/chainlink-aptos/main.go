d2d710f5a04815ac6d7946e13fde43b5b217dce2
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2024-09-05 04:00:38 +0200

    Aptos TOML config validation

--- relayer/cmd/chainlink-aptos/main.go
+++ relayer/cmd/chainlink-aptos/main.go
@@ -59,10 +59,21 @@ type pluginRelayer struct {
 func (c *pluginRelayer) NewRelayer(ctx context.Context, rawConfig string, loopKs loop.Keystore, capRegistry core.CapabilitiesRegistry) (loop.Relayer, error) {
 	d := toml.NewDecoder(strings.NewReader(rawConfig))
 	d.DisallowUnknownFields()
+
 	var cfg config.TOMLConfig
+
 	if err := d.Decode(&cfg); err != nil {
 		return nil, fmt.Errorf("failed to decode config toml: %w:\n\t%s", err, rawConfig)
 	}
+
+	if err := cfg.ValidateConfig(); err != nil {
+		return nil, fmt.Errorf("invalid aptos config: %w", err)
+	}
+
+	if !cfg.IsEnabled() {
+		return nil, fmt.Errorf("cannot create new chain with ID %s: config is disabled", cfg.ChainID)
+	}
+
 	cfg.SetDefaults()
 
 	opts := chain.ChainOpts{
