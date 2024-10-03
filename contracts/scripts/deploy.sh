aba2c3ca46d9858fd04b8fc2f08969d9b8143817
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2024-10-03 13:44:18 +0200

    Fix Aptos deploy script - DF registry config

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -8,7 +8,7 @@ KEYSTONE_ADDR=$(cat keystone/contract_address.txt)
 DATA_FEEDS_ADDR=$(cat data-feeds/contract_address.txt)
 
 # data_feeds::router::set_workflow_config(workflow_owners, workflow_names)
-aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_workflow_config" --assume-yes --args 'string:["0000FOOBAR"]' 'string:[]'
+aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_workflow_config" --assume-yes --args 'hex:["0x00000000000000000000000000000000000000aa"]' 'string:["0000FOOBAR"]'
 
 # data_feeds::router::set_feeds(feed_ids, descriptions, config_id)
 aptos move run --function-id "$DATA_FEEDS_ADDR::registry::set_feeds" --assume-yes --args 'hex:["0x0003111111111111111100000000000000000000000000000000000000000000"]' 'string:["FOOBAR"]' 'hex:0x99'
