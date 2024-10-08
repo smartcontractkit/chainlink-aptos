2981e74ed88d0ec27280f0b5466db8a5dfdcff20
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2024-10-08 11:12:38 +0200

    Update chainlink core Docker build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 
 pushd "$(dirname -- "$0")/../../../chainlink"
 # Attempt to build the Docker image
-if docker build . -t smartcontract/chainlink:${CONTAINER_VERSION} -f ./plugins/chainlink.Dockerfile --build-arg MODE=dev; then
+if docker build . -t smartcontract/chainlink:${CONTAINER_VERSION} -f ./plugins/chainlink.Dockerfile --build-arg TAGS=dev; then
     echo "Docker build successful."
 fi
 
