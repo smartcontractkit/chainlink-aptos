7f5fc284bebfd110acf0ee9be5471d899f6d249a
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-01-29 15:40:56 +0100

    Update aptos CLI pkg to 6.0.2

--- aptos.nix
+++ aptos.nix
@@ -7,13 +7,11 @@
 }:
 stdenv.mkDerivation rec {
   name = "aptos-${version}";
-  version = "4.1.0";
+  version = "6.0.2";
 
-  # Do we need to upgrade the CLI version to make the tests pass?
-  # MISSING_DEPENDENCY (code 1021) error with error message: "Missing Native Function `move_range`".
   src = fetchzip {
     url = "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v${version}/aptos-cli-${version}-Ubuntu-22.04-x86_64.zip";
-    sha256 = "sha256-8PTE/6kaq3dnU/bdgfofAQ8RN6otAoq7Vi4xXXP1Z8M=";
+    sha256 = "sha256-k2E53j3dvqNwFJrpVvjcSXEqAD6Nu4bzuMJTWWBupBU=";
   };
 
   nativeBuildInputs = [
