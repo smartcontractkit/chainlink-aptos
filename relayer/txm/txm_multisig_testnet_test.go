da8e19d044027bf568f9e0a78b090f197fdf6c19
cfal <cfal@users.noreply.github.com>
2025-02-07 23:41:04 +0400

    aptos/relayer/txm/txm_multisig_testnet_test.go: add testnet tests

---
+++ relayer/txm/txm_multisig_testnet_test.go
@@ -0,0 +1,39 @@
+//go:build integration && testnet
+
+package txm
+
+import (
+	"testing"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+
+	"github.com/smartcontractkit/chainlink-internal-integrations/aptos/relayer/testutils"
+)
+
+func TestMultisigDevnet(t *testing.T) {
+	runMultisigTestnetTest(t, testutils.DevnetUrl)
+}
+
+func TestMultisigTestnet(t *testing.T) {
+	runMultisigTestnetTest(t, testutils.TestnetUrl)
+}
+
+func runMultisigTestnetTest(t *testing.T, rpcUrl string) {
+	logger := logger.Test(t)
+
+	privateKey, publicKey, accountAddress := testutils.LoadAccountFromEnv(t, logger)
+	if privateKey == nil {
+		t.Fatal("PRIVATE_KEY or ADDRESS environment variable is not set")
+	}
+
+	keystore := testutils.NewTestKeystore(t)
+	keystore.AddKey(privateKey)
+
+	account := Account{
+		privateKey:     privateKey,
+		publicKey:      publicKey,
+		accountAddress: accountAddress,
+	}
+
+	runMultisigTest(t, logger, rpcUrl, keystore, []Account{account}, true)
+}
