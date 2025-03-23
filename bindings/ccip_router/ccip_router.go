7762afadb535ca0c28c15be492788ceebf243314
cfal <cfal@users.noreply.github.com>
2025-03-23 09:29:43 +0400

    bindings/ccip_router/ccip_router.go: add DeployToExistingObject

--- bindings/ccip_router/ccip_router.go
+++ bindings/ccip_router/ccip_router.go
@@ -2,6 +2,7 @@ package cciprouter
 
 import (
 	"github.com/aptos-labs/aptos-go-sdk"
+	"github.com/aptos-labs/aptos-go-sdk/api"
 
 	"github.com/smartcontractkit/chainlink-aptos/bindings/bind"
 	module_router "github.com/smartcontractkit/chainlink-aptos/bindings/ccip_router/router"
@@ -22,14 +23,11 @@ var FunctionInfo = bind.MustParseFunctionInfo(
 	module_router.FunctionInfo,
 )
 
-func Compile(CCIPAddress aptos.AccountAddress, mcmsAddress aptos.AccountAddress, registerMCMSEntrypoints bool) (compile.CompiledPackage, error) {
+func Compile(ccipAddress aptos.AccountAddress) (compile.CompiledPackage, error) {
 	namedAddresses := map[string]aptos.AccountAddress{
-		"ccip":                      CCIPAddress,
-		"mcms":                      mcmsAddress,
+		"ccip":                      ccipAddress,
+		"mcms":                      aptos.AccountZero,
 		"mcms_register_entrypoints": aptos.AccountZero,
-	}
-	if registerMCMSEntrypoints {
-		namedAddresses["mcms_register_entrypoints"] = CCIPAddress
 	}
 	// Compile using CLI
 	return compile.CompilePackage("ccip_router", namedAddresses)
@@ -44,4 +42,25 @@ func Bind(address aptos.AccountAddress, client aptos.AptosRpcClient) CCIPRouter 
 			RouterTransactor: module_router.RouterTransactor{BoundContract: router},
 		},
 	}
+}
+
+// DeployToExistingObject deploys the CCIP router package to an existing code object.
+// This should not be used in production, where CCIP is deployed via MCMS.
+func DeployToExistingObject(
+	auth aptos.TransactionSigner,
+	client aptos.AptosRpcClient,
+	objectAddress aptos.AccountAddress,
+	ccipAddress aptos.AccountAddress,
+) (*api.PendingTransaction, CCIPRouter, error) {
+	// no need for mcms addresses since the router does not interact with mcms directly.
+	namedAddresses := map[string]aptos.AccountAddress{
+		"ccip":                      ccipAddress,
+		"mcms":                      aptos.AccountZero,
+		"mcms_register_entrypoints": aptos.AccountZero,
+	}
+	tx, err := bind.UpgradePackageToObject(auth, client, "ccip_router", namedAddresses, objectAddress)
+	if err != nil {
+		return nil, CCIPRouter{}, err
+	}
+	return tx, Bind(objectAddress, client), nil
 }
