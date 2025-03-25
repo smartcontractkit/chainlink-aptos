3f00198e61d2f22a69d291b4fa72b7bb708e604d
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2025-03-25 16:23:22 +0100

    Simplify address arguments

--- bindings/ccip_router/ccip_router.go
+++ bindings/ccip_router/ccip_router.go
@@ -23,7 +23,7 @@ var FunctionInfo = bind.MustParseFunctionInfo(
 	module_router.FunctionInfo,
 )
 
-func Compile(ccipAddress aptos.AccountAddress, mcmsAddress aptos.AccountAddress) (compile.CompiledPackage, error) {
+func Compile(ccipAddress, mcmsAddress aptos.AccountAddress) (compile.CompiledPackage, error) {
 	namedAddresses := map[string]aptos.AccountAddress{
 		"ccip":                      ccipAddress,
 		"ccip_router":               ccipAddress,
@@ -50,9 +50,7 @@ func Bind(address aptos.AccountAddress, client aptos.AptosRpcClient) CCIPRouter 
 func DeployToExistingObject(
 	auth aptos.TransactionSigner,
 	client aptos.AptosRpcClient,
-	objectAddress aptos.AccountAddress,
-	ccipAddress aptos.AccountAddress,
-	mcmsAddress aptos.AccountAddress,
+	objectAddress, ccipAddress, mcmsAddress aptos.AccountAddress,
 ) (*api.PendingTransaction, CCIPRouter, error) {
 	// no need for mcms addresses since the router does not interact with mcms directly.
 	namedAddresses := map[string]aptos.AccountAddress{
