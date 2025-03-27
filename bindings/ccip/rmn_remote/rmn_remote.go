ccfb51753d1f9e4d3cd901e14716ac8f9cd9cd61
cfal <cfal@users.noreply.github.com>
2025-03-27 21:20:41 +0400

    bindings/ccip: regenerate

--- bindings/ccip/rmn_remote/rmn_remote.go
+++ bindings/ccip/rmn_remote/rmn_remote.go
@@ -23,7 +23,7 @@ var (
 
 type RMNRemote interface {
 	TypeAndVersion(opts *bind.CallOpts) (string, error)
-	Verify(opts *bind.CallOpts, merkleRootSourceChainSelectors []uint64, merkleRootMinSequenceNumbers []uint64, merkleRootMaxSequenceNumbers []uint64, merkleRootValues [][]byte, signatures [][]byte) (bool, error)
+	Verify(opts *bind.CallOpts, merkleRootSourceChainSelectors []uint64, merkleRootMinSeqNrs []uint64, merkleRootMaxSeqNrs []uint64, merkleRootValues [][]byte, signatures [][]byte) (bool, error)
 	GetVersionedConfig(opts *bind.CallOpts) (uint32, Config, error)
 	GetLocalChainSelector(opts *bind.CallOpts) (uint64, error)
 	GetReportDigestHeader(opts *bind.CallOpts) ([]byte, error)
@@ -41,7 +41,7 @@ type RMNRemote interface {
 
 type RMNRemoteEncoder interface {
 	TypeAndVersion() (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
-	Verify(merkleRootSourceChainSelectors []uint64, merkleRootMinSequenceNumbers []uint64, merkleRootMaxSequenceNumbers []uint64, merkleRootValues [][]byte, signatures [][]byte) (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
+	Verify(merkleRootSourceChainSelectors []uint64, merkleRootMinSeqNrs []uint64, merkleRootMaxSeqNrs []uint64, merkleRootValues [][]byte, signatures [][]byte) (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
 	GetVersionedConfig() (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
 	GetLocalChainSelector() (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
 	GetReportDigestHeader() (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error)
@@ -97,8 +97,8 @@ type Report struct {
 
 type MerkleRoot struct {
 	SourceChainSelector uint64 `move:"u64"`
-	MinSequenceNumber   uint64 `move:"u64"`
-	MaxSequenceNumber   uint64 `move:"u64"`
+	MinSeqNr            uint64 `move:"u64"`
+	MaxSeqNr            uint64 `move:"u64"`
 	MerkleRoot          []byte `move:"vector<u8>"`
 }
 
@@ -152,8 +152,8 @@ func (c RMNRemoteContract) TypeAndVersion(opts *bind.CallOpts) (string, error) {
 	return r0, nil
 }
 
-func (c RMNRemoteContract) Verify(opts *bind.CallOpts, merkleRootSourceChainSelectors []uint64, merkleRootMinSequenceNumbers []uint64, merkleRootMaxSequenceNumbers []uint64, merkleRootValues [][]byte, signatures [][]byte) (bool, error) {
-	module, function, typeTags, args, err := c.rmnRemoteEncoder.Verify(merkleRootSourceChainSelectors, merkleRootMinSequenceNumbers, merkleRootMaxSequenceNumbers, merkleRootValues, signatures)
+func (c RMNRemoteContract) Verify(opts *bind.CallOpts, merkleRootSourceChainSelectors []uint64, merkleRootMinSeqNrs []uint64, merkleRootMaxSeqNrs []uint64, merkleRootValues [][]byte, signatures [][]byte) (bool, error) {
+	module, function, typeTags, args, err := c.rmnRemoteEncoder.Verify(merkleRootSourceChainSelectors, merkleRootMinSeqNrs, merkleRootMaxSeqNrs, merkleRootValues, signatures)
 	if err != nil {
 		return *new(bool), err
 	}
@@ -302,7 +302,7 @@ func (c rmnRemoteEncoder) TypeAndVersion() (bind.ModuleInformation, string, []ap
 	return c.BoundContract.Encode("type_and_version", nil, []string{}, []any{})
 }
 
-func (c rmnRemoteEncoder) Verify(merkleRootSourceChainSelectors []uint64, merkleRootMinSequenceNumbers []uint64, merkleRootMaxSequenceNumbers []uint64, merkleRootValues [][]byte, signatures [][]byte) (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error) {
+func (c rmnRemoteEncoder) Verify(merkleRootSourceChainSelectors []uint64, merkleRootMinSeqNrs []uint64, merkleRootMaxSeqNrs []uint64, merkleRootValues [][]byte, signatures [][]byte) (bind.ModuleInformation, string, []aptos.TypeTag, [][]byte, error) {
 	return c.BoundContract.Encode("verify", nil, []string{
 		"vector<u64>",
 		"vector<u64>",
@@ -311,8 +311,8 @@ func (c rmnRemoteEncoder) Verify(merkleRootSourceChainSelectors []uint64, merkle
 		"vector<vector<u8>>",
 	}, []any{
 		merkleRootSourceChainSelectors,
-		merkleRootMinSequenceNumbers,
-		merkleRootMaxSequenceNumbers,
+		merkleRootMinSeqNrs,
+		merkleRootMaxSeqNrs,
 		merkleRootValues,
 		signatures,
 	})
