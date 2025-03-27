ae60f1d896d20d891d41971b8c67b7afae1eb571
cfal <cfal@users.noreply.github.com>
2025-03-27 10:47:28 +0400

    relayer: update mapstructure package

--- relayer/chainreader/chainreader.go
+++ relayer/chainreader/chainreader.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 
 	"github.com/aptos-labs/aptos-go-sdk"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
