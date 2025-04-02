501823120f77a2798498b3b7a131694239c2902b
cfal <cfal@users.noreply.github.com>
2025-04-02 12:09:44 +0900

    relayer/chainreader: add block info to QueryKey results (#70)
    * relayer/chainreader/chainreader.go: add block info to QueryKey Head struct
    
    * go.mod: update aptos-go-sdk for event.Version field
    
    * go.sum: update

--- relayer/chainreader/chainreader.go
+++ relayer/chainreader/chainreader.go
@@ -3,6 +3,7 @@ package chainreader
 import (
 	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"reflect"
@@ -264,6 +265,7 @@ func (a *aptosChainReader) QueryKey(ctx context.Context, contract types.BoundCon
 	contractName := contract.Name
 
 	address, ok := a.moduleAddresses[contractName]
+
 	if !ok {
 		return nil, fmt.Errorf("no bound address for module %s", contractName)
 	}
@@ -283,7 +285,7 @@ func (a *aptosChainReader) QueryKey(ctx context.Context, contract types.BoundCon
 		expressions = convertedExpressions
 	}
 
-	// temp: parsing offset from queryFilter because limitAndSort doesn't support offset-based pagination
+	// TODO: parsing offset from queryFilter because limitAndSort doesn't support offset-based pagination
 	var eventOffset uint64 = 0
 	for _, expr := range expressions {
 		if expr.IsPrimitive() {
@@ -402,7 +404,8 @@ func (a *aptosChainReader) QueryKey(ctx context.Context, contract types.BoundCon
 		}
 	}
 
-	var sequences []types.Sequence
+	sequences := []types.Sequence{}
+	headCache := map[uint64]types.Head{}
 	for _, event := range events {
 		jsonData := event.Data
 
@@ -410,19 +413,47 @@ func (a *aptosChainReader) QueryKey(ctx context.Context, contract types.BoundCon
 			return nil, fmt.Errorf("failed to rename event fields: %+w", err)
 		}
 
-		// create new instance of eventData for each event
-		eventData := reflect.New(reflect.TypeOf(sequenceDataType).Elem()).Interface()
+		var eventData any
+		if a.config.IsLoopPlugin {
+			// immediately remarshal the data
+			// TODO: update aptos-go-sdk to allow returning the string directly
+			resultBytes, err := json.Marshal(jsonData)
+			if err != nil {
+				return nil, fmt.Errorf("failed to re-marshal event for seqNum %d: %w", event.SequenceNumber, err)
+			}
+			eventData = resultBytes
+		} else {
+			// create new instance of eventData for each event
+			eventData = reflect.New(reflect.TypeOf(sequenceDataType).Elem()).Interface()
 
-		err := codec.DecodeAptosJsonValue(jsonData, &eventData)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode event data: %+w", err)
+			err := codec.DecodeAptosJsonValue(jsonData, &eventData)
+			if err != nil {
+				return nil, fmt.Errorf("failed to decode event data: %+w", err)
+			}
 		}
 
+		head, ok := headCache[event.Version]
+		if !ok {
+			block, err := a.client.BlockByVersion(event.Version, false)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get block by version: %w", err)
+			}
+			hexBytes, err := hex.DecodeString(strings.TrimPrefix(block.BlockHash, "0x"))
+			if err != nil {
+				return nil, fmt.Errorf("failed to decode block hash: %w", err)
+			}
+			head = types.Head{
+				Height: fmt.Sprintf("%d", block.BlockHeight),
+				Hash:   hexBytes,
+				// microseconds to seconds
+				Timestamp: block.BlockTimestamp / 1000000,
+			}
+			headCache[event.Version] = head
+		}
 		sequence := types.Sequence{
 			Cursor: fmt.Sprintf("%d", event.SequenceNumber),
-			// todo: enrich with block data?
-			Head: types.Head{},
-			Data: eventData,
+			Head:   head,
+			Data:   eventData,
 		}
 		sequences = append(sequences, sequence)
 	}
@@ -511,6 +542,7 @@ func (a *aptosChainReader) Bind(ctx context.Context, bindings []types.BoundContr
 }
 
 func (a *aptosChainReader) Unbind(ctx context.Context, bindings []types.BoundContract) error {
+
 	for _, binding := range bindings {
 		key := binding.Name
 		if _, ok := a.moduleAddresses[key]; ok {
