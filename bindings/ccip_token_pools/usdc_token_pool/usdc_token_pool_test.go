aa809d6ae7b420282520e88b47155711d5c5c462
JohnChangUK <johnchang9094@gmail.com>
2025-04-15 11:41:04 -0400

    Change addresses to fix duplicate address and module error

--- bindings/ccip_token_pools/usdc_token_pool/usdc_token_pool_test.go
+++ bindings/ccip_token_pools/usdc_token_pool/usdc_token_pool_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestCompile(t *testing.T) {
 	t.Parallel()
-	output, err := Compile(aptos.AccountOne, aptos.AccountOne, aptos.AccountOne, aptos.AccountOne, aptos.AccountOne, aptos.AccountOne, aptos.AccountTwo, aptos.AccountThree, aptos.AccountOne, aptos.AccountOne, false)
+	output, err := Compile(aptos.AccountThree, aptos.AccountThree, aptos.AccountFour, aptos.AccountTen, aptos.AccountTen, aptos.AccountOne, aptos.AccountTwo, aptos.AccountFour, aptos.AccountOne, aptos.AccountOne, false)
 	require.NoError(t, err)
 	require.NotZero(t, output.Metadata, "Compilation resulted in no metadata")
 	require.NotZero(t, output.Bytecode, "Compilation resulted in no bytecode")
