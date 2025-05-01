945bdaf6d8c5e7a9687f6fdb023fa13c68ce48c8
Karen Stepanyan <karen.stepanyan@smartcontract.com>
2025-05-01 19:30:54 +0400

    add platform bindings

---
+++ bindings/data_feeds/data_feeds.go
@@ -0,0 +1,81 @@
+package data_feeds
+
+import (
+	"github.com/aptos-labs/aptos-go-sdk"
+	"github.com/aptos-labs/aptos-go-sdk/api"
+	"github.com/smartcontractkit/chainlink-aptos/bindings/bind"
+	"github.com/smartcontractkit/chainlink-aptos/bindings/compile"
+	module_registry "github.com/smartcontractkit/chainlink-aptos/bindings/data_feeds/registry"
+	module_router "github.com/smartcontractkit/chainlink-aptos/bindings/data_feeds/router"
+	"github.com/smartcontractkit/chainlink-aptos/contracts"
+)
+
+type DataFeeds interface {
+	Address() aptos.AccountAddress
+
+	Registry() module_registry.RegistryInterface
+	Router() module_router.RouterInterface
+}
+
+var _ DataFeeds = DataFeedsContract{}
+
+type DataFeedsContract struct {
+	address aptos.AccountAddress
+
+	registry module_registry.RegistryInterface
+	router   module_router.RouterInterface
+}
+
+func (C DataFeedsContract) Address() aptos.AccountAddress {
+	return C.address
+}
+
+func (C DataFeedsContract) Registry() module_registry.RegistryInterface {
+	return C.registry
+}
+func (C DataFeedsContract) Router() module_router.RouterInterface {
+	return C.router
+}
+
+var FunctionInfo = bind.MustParseFunctionInfo(
+	module_registry.FunctionInfo,
+	module_router.FunctionInfo,
+)
+
+func Compile(address aptos.AccountAddress, owner aptos.AccountAddress, platform aptos.AccountAddress) (compile.CompiledPackage, error) {
+	namedAddresses := map[string]aptos.AccountAddress{
+		"data_feeds": address,
+		"owner":      owner,
+		"platform":   platform,
+	}
+	// Compile using CLI
+	return compile.CompilePackage(contracts.DataFeeds, namedAddresses)
+}
+
+func Bind(address aptos.AccountAddress, client aptos.AptosRpcClient) DataFeeds {
+	return DataFeedsContract{
+		address:  address,
+		registry: module_registry.NewRegistry(address, client),
+		router:   module_router.NewRouter(address, client),
+	}
+}
+
+// DeployToObject deploys the Data Feeds contract to a new named object.
+// The resulting address will be calculated using the deployer's account address and the next sequence number
+func DeployToObject(
+	auth aptos.TransactionSigner,
+	client aptos.AptosRpcClient,
+	address aptos.AccountAddress,
+	platform aptos.AccountAddress,
+) (aptos.AccountAddress, *api.PendingTransaction, DataFeeds, error) {
+	namedAddresses := map[string]aptos.AccountAddress{
+		"data_feeds": address,
+		"owner":      address,
+		"platform":   platform,
+	}
+	address, tx, err := bind.DeployPackageToObject(auth, client, contracts.DataFeeds, namedAddresses)
+	if err != nil {
+		return aptos.AccountAddress{}, nil, nil, err
+	}
+	return address, tx, Bind(address, client), nil
+}
