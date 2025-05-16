e2008e7179ed244d8eda7ebc98ebcd510ca8d745
Dmytro Haidashenko <34754799+dhaidashenko@users.noreply.github.com>
2025-05-16 13:43:02 +0200

    Allow appending tag to the write target ID (#164)

--- relayer/write_target/aptos/write_target.go
+++ relayer/write_target/aptos/write_target.go
@@ -28,7 +28,7 @@ func NewAptosWriteTarget(ctx context.Context, chain chain.Chain, lggr logger.Log
 	// chainName, err := chainselectors.NameFromChainId(chain.ID().Uint64())
 
 	// Construct the ID for the WT (e.g., "write_aptos-localnet@1.0.0")
-	id, err := write_target.NewWriteTargetID(aptosconfig.ChainFamilyName, config.NetworkName, config.ChainID, version)
+	id, err := write_target.NewWriteTargetID(aptosconfig.ChainFamilyName, config.NetworkName, config.ChainID, config.WriteTargetCap.Tag, version)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create write target ID: %+w", err)
 	}
