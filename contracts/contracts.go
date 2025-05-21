455ee33f955ff0117812c9b8ed589a68588c72c5
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2025-05-21 18:43:26 +0200

    gofmt

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -11,17 +11,17 @@ var Embed embed.FS
 type Package string
 
 const (
-	CCIP                = Package("ccip")
-	CCIPPingPongDemo    = Package("ccip_ping_pong_demo")
-	CCIPBurnMintPool    = Package("burn_mint_token_pool")
-	CCIPLockReleasePool = Package("lock_release_token_pool")
-	CCIPUSDCTokenPool   = Package("usdc_token_pool")
+	CCIP                 = Package("ccip")
+	CCIPPingPongDemo     = Package("ccip_ping_pong_demo")
+	CCIPBurnMintPool     = Package("burn_mint_token_pool")
+	CCIPLockReleasePool  = Package("lock_release_token_pool")
+	CCIPUSDCTokenPool    = Package("usdc_token_pool")
 	CCIPManagedTokenPool = Package("managed_token_pool")
-	CCIPTokenPool       = Package("ccip_token_pool")
-	CCIPRouter          = Package("ccip_router")
-	CCIPDummyReceiver   = Package("ccip_dummy_receiver")
-	CCIPOfframp         = Package("ccip_offramp")
-	CCIPOnramp          = Package("ccip_onramp")
+	CCIPTokenPool        = Package("ccip_token_pool")
+	CCIPRouter           = Package("ccip_router")
+	CCIPDummyReceiver    = Package("ccip_dummy_receiver")
+	CCIPOfframp          = Package("ccip_offramp")
+	CCIPOnramp           = Package("ccip_onramp")
 
 	ManagedToken  = Package("link")
 	MCMSRegistrar = Package("mcms-registrar")
@@ -37,19 +37,19 @@ const (
 
 // Contracts maps packages to their respective root directories within Embed
 var Contracts map[Package]string = map[Package]string{
-	CCIP:                filepath.Join("ccip", "ccip"),
-	CCIPPingPongDemo:    filepath.Join("ccip", "ccip_ping_pong_demo"),
-	CCIPBurnMintPool:    filepath.Join("ccip", "ccip_token_pools", "burn_mint_token_pool"),
-	CCIPLockReleasePool: filepath.Join("ccip", "ccip_token_pools", "lock_release_token_pool"),
-	CCIPUSDCTokenPool:   filepath.Join("ccip", "ccip_token_pools", "usdc_token_pool"),
+	CCIP:                 filepath.Join("ccip", "ccip"),
+	CCIPPingPongDemo:     filepath.Join("ccip", "ccip_ping_pong_demo"),
+	CCIPBurnMintPool:     filepath.Join("ccip", "ccip_token_pools", "burn_mint_token_pool"),
+	CCIPLockReleasePool:  filepath.Join("ccip", "ccip_token_pools", "lock_release_token_pool"),
+	CCIPUSDCTokenPool:    filepath.Join("ccip", "ccip_token_pools", "usdc_token_pool"),
 	CCIPManagedTokenPool: filepath.Join("ccip", "ccip_token_pools", "managed_token_pool"),
-	CCIPTokenPool:       filepath.Join("ccip", "ccip_token_pools", "token_pool"),
-	CCIPRouter:          filepath.Join("ccip", "ccip_router"),
-	CCIPDummyReceiver:   filepath.Join("ccip", "ccip_dummy_receiver"),
-	CCIPOfframp:         filepath.Join("ccip", "ccip_offramp"),
-	CCIPOnramp:          filepath.Join("ccip", "ccip_onramp"),
-	DataFeeds:           "data-feeds",
-	Platform:            "platform",
+	CCIPTokenPool:        filepath.Join("ccip", "ccip_token_pools", "token_pool"),
+	CCIPRouter:           filepath.Join("ccip", "ccip_router"),
+	CCIPDummyReceiver:    filepath.Join("ccip", "ccip_dummy_receiver"),
+	CCIPOfframp:          filepath.Join("ccip", "ccip_offramp"),
+	CCIPOnramp:           filepath.Join("ccip", "ccip_onramp"),
+	DataFeeds:            "data-feeds",
+	Platform:             "platform",
 
 	ManagedToken:  filepath.Join("managed_token"),
 	MCMSRegistrar: filepath.Join("mcms-registrar"),
