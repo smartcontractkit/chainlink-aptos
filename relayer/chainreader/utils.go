cd9f33c6ca0b238951ae9089604b371e7595ca19
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2025-06-02 22:46:08 +0200

    Increasing API read batch size + Events by creation number (#198)
    * Increasing API read batch size
    
    * Fetching events by creation number
    
    * Bumping up aptos go sdk version

--- relayer/chainreader/utils.go
+++ relayer/chainreader/utils.go
@@ -3,6 +3,7 @@ package chainreader
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
@@ -83,6 +84,41 @@ func renameMapFields(jsonData map[string]any, renames map[string]RenamedField) e
 		}
 	}
 	return nil
+}
+
+func extractEventCreationNum(resourceData map[string]any, eventFieldPath string) (string, error) {
+	pathComponents := strings.Split(eventFieldPath, ".")
+
+	current, ok := resourceData["data"].(map[string]any)
+	if !ok {
+		return "", fmt.Errorf("resource data missing 'data' field or not a map")
+	}
+
+	for i, component := range pathComponents {
+		nextLevel, ok := current[component].(map[string]any)
+		if !ok {
+			return "", fmt.Errorf("cannot navigate path at component %s (position %d): field missing or not a map",
+				component, i)
+		}
+		current = nextLevel
+	}
+
+	guid, ok := current["guid"].(map[string]any)
+	if !ok {
+		return "", fmt.Errorf("event field missing 'guid' structure")
+	}
+
+	id, ok := guid["id"].(map[string]any)
+	if !ok {
+		return "", fmt.Errorf("event field missing 'guid.id' structure")
+	}
+
+	creationNum, ok := id["creation_num"].(string)
+	if !ok {
+		return "", fmt.Errorf("event field missing 'guid.id.creation_num' value or not a string")
+	}
+
+	return creationNum, nil
 }
 
 func compareValue(fieldValue, compareValue any, operator primitives.ComparisonOperator) bool {
