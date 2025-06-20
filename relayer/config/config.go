f279595f09ba6461f84193b45440d70a083cad5d
cfal <cfal@users.noreply.github.com>
2025-06-20 23:01:34 +0900

    relayer/config/config.go: remove unused TOMLConfigs

--- relayer/config/config.go
+++ relayer/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"slices"
 	"strings"
 	"time"
@@ -66,62 +65,6 @@ func (n *Node) ValidateConfig() (err error) {
 
 	return
 }
-
-type TOMLConfigs []*TOMLConfig
-
-func (cs TOMLConfigs) ValidateConfig() (err error) {
-	return cs.validateKeys()
-}
-
-func (cs TOMLConfigs) validateKeys() (err error) {
-	// Unique chain IDs
-	chainIDs := config.UniqueStrings{}
-	for i, c := range cs {
-		if chainIDs.IsDupe(&c.ChainID) {
-			err = errors.Join(err, config.NewErrDuplicate(fmt.Sprintf("%d.ChainID", i), c.ChainID))
-		}
-	}
-
-	// Unique node names
-	names := config.UniqueStrings{}
-	for i, c := range cs {
-		for j, n := range c.Nodes {
-			if names.IsDupe(n.Name) {
-				err = errors.Join(err, config.NewErrDuplicate(fmt.Sprintf("%d.Nodes.%d.Name", i, j), *n.Name))
-			}
-		}
-	}
-
-	// Unique URLs
-	urls := config.UniqueStrings{}
-	for i, c := range cs {
-		for j, n := range c.Nodes {
-			u := (*url.URL)(n.URL)
-			if urls.IsDupeFmt(u) {
-				err = errors.Join(err, config.NewErrDuplicate(fmt.Sprintf("%d.Nodes.%d.URL", i, j), u.String()))
-			}
-		}
-	}
-	return
-}
-
-// func (cs *TOMLConfigs) SetFrom(fs *TOMLConfigs) (err error) {
-// 	if err1 := fs.validateKeys(); err1 != nil {
-// 		return err1
-// 	}
-// 	for _, f := range *fs {
-// 		if f.ChainID == nil {
-// 			*cs = append(*cs, f)
-// 		} else if i := slices.IndexFunc(*cs, func(c *TOMLConfig) bool {
-// 			return c.ChainID != nil && *c.ChainID == *f.ChainID
-// 		}); i == -1 {
-// 			*cs = append(*cs, f)
-// 		} else {
-// 			(*cs)[i].SetFrom(f)
-// 		}
-// 	}
-// 	return
-// }
 
 type TOMLConfig struct {
 	// Do not access directly. Use [IsEnabled]
