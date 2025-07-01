1ac559e61f17669e4b2f5c815672b9985ae3b8cc
JohnChangUK <johnchang9094@gmail.com>
2025-07-01 16:45:35 +0100

    Validate report MessageID length 32

--- relayer/chainreader/utils/msghasher.go
+++ relayer/chainreader/utils/msghasher.go
@@ -67,6 +67,10 @@ func (h *MessageHasherV1) Hash(ctx context.Context, report *ExecutionReport, onR
 		return [32]byte{}, fmt.Errorf("compute metadata hash: %w", err)
 	}
 
+	if len(report.Message.Header.MessageID) != 32 {
+		return [32]byte{}, fmt.Errorf("invalid MessageID length: %d", len(report.Message.Header.MessageID))
+	}
+
 	var messageID [32]byte
 	copy(messageID[:], report.Message.Header.MessageID)
 
