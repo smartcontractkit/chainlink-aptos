f2fc29f599bcdba0d9a5dc48cb38359c9e063d86
JohnChangUK <johnchang9094@gmail.com>
2026-01-05 21:37:36 -0500

    CurseMCMS package to call CCIP RMN curse ops

--- gen.go
+++ gen.go
@@ -38,6 +38,8 @@ package aptos
 //go:generate go run ./cmd/bindgen --input ./contracts/mcms/mcms/sources/mcms_executor.move --output ./bindings/mcms/mcms_executor
 //go:generate go run ./cmd/bindgen --input ./contracts/mcms/mcms/sources/mcms_registry.move --output ./bindings/mcms/mcms_registry
 
+//go:generate go run ./cmd/bindgen --input ./contracts/mcms/curse_mcms/sources/curse_mcms.move --output ./bindings/curse_mcms/curse_mcms
+
 //go:generate go run ./cmd/bindgen --input ./contracts/mcms/mcms_test/sources/mcms_user.move --output ./bindings/mcms_test/mcms_user
 
 //go:generate go run ./cmd/bindgen --input ./contracts/data-feeds/sources/registry.move --output ./bindings/data_feeds/registry
