31384b9ed53683fe58c3fc37cb5aebbc482c5b8a
cawthorne <cawthornegd@gmail.com>
2026-02-26 22:37:44 +0000

    aptos: route SubmitTransaction through txm and fix mempool sequence collisions

--- relayer/txm/tx.go
+++ relayer/txm/tx.go
@@ -20,8 +20,18 @@ type AptosTx struct {
 	FunctionName    string
 	TypeTags        []aptos.TypeTag
 	BcsValues       [][]byte
-	Attempt         uint64
-	Status          commontypes.TransactionStatus
-	Simulate        bool
-	Fee             *big.Int // Transaction fee in octas (1e-8 APT)
+	// Payload, when set, is used directly instead of reconstructing an EntryFunction from ModuleName/FunctionName/Args.
+	Payload              *aptos.TransactionPayload
+	GasUnitPriceOverride *uint64
+	Attempt              uint64
+	Status               commontypes.TransactionStatus
+	Simulate             bool
+	Fee                  *big.Int // Transaction fee in octas (1e-8 APT)
+
+	LastSubmittedHash                    string
+	LastSubmittedNonce                   uint64
+	LastSubmittedExpirationTimestampSecs uint64
+	LastSubmittedMaxGasAmount            uint64
+	LastSubmittedGasUnitPrice            uint64
+	LastSubmittedSignature               []byte
 }
